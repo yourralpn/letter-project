25de3d3d6b0ae875624127345d5c7c9d2bcd8551
yourralpn <yourralpn@gmail.com>
2025-09-30 05:51:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const message = `
 Hi, Aina Silvia Monicha.
-
 Selamat ulang tahun yang ke-17 ya!.
-
 Pada hari ini, segala doa yang terbaik ku langitkan hanya untukmu. Semoga segala hal yang membuatmu terjatuh, menjadi alasan kamu untuk selalu bertumbuh. Semoga dunia senantiasa menjaga dirimu di mana pun kamu berada.
-
 Meskipun tak ada kue tart yang bernyalakan lilin dariku, jangan padamkan segala doa terbaik yang kupanjatkan untukmu.
-
 Berbahagialah. 
-
 Sweet Seventeen, Aina Silvia Monicha...`;
 
 function showLetter() {
@@ -34,4 +29,5 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
